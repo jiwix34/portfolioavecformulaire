5a4012fa522710a560d8f3d9410784eab2764a3d
jiwix34 <friedblattjimmybeweb@outlook.fr>
2016-09-30 11:23:12 +0200

    initialisation premier commit formulaire js css et structure

---
+++ index.php
@@ -0,0 +1,227 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, , minimum-scale=1, user-scalable=no">
+    <link rel="shortcut icon" href="./css/img/logo.png"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+    <link href='https://fonts.googleapis.com/css?family=Patua+One' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Black+Ops+One" rel="stylesheet">
+    <link rel="stylesheet" href="./css/animate.css"/>
+    <link rel="stylesheet" href="./css/main.css"/>
+
+
+    <title> Portfolio jimmy </title>
+  </head>
+  <body>
+
+    <nav class=" col-xs-12 containernav">
+        <ul class="groupelistenav">
+          <li class="listenav listenav1"><a class="js-scrollTo" href="#quisuisje">Qui suis-je</a></li>
+          <li class="listenav listenav1"><a class="js-scrollTo" href="#formation">Formation</a></li>
+          <!--<li class="listenav"><a href="#projet">Projet</a></li>-->
+          <li class="listenav listenav2"><a class="js-scrollTo" href="#competences">Competences</a></li>
+          <li class="listenav listenav2"><a class="js-scrollTo" href="#contact">Contact</a></li>
+        </ul>
+    </nav>
+
+    <div class=" entete">
+      <header id="fondtete">
+        <h1>
+          <p>Jimmy</p>
+          <p>Developpeur web</p>
+        </h1>
+      </header>
+    </div>
+
+    <section id="quisuisje" class="toutesection">
+      <header>
+        <h2 class="wow bounceInRight" data-wow-duration="3s" data-wow-delay="100ms">Jimmy Friedblatt</h2>
+      </header>
+      <article class="contenue wow bounceInLeft" data-wow-duration="4s" data-wow-delay="100ms">
+        <p >Passionné par le développement web, j'ai intégré l'école numérique beWeb
+       créée par Fondespierre en partenariat avec Simplon.co.
+        Actuellement, j'apprends plusieurs langages: HTML, CSS, JAVASCRIPT, PHP, MYSQL, etc...</p>
+        <p>Mon rêve : développer un jour aux États-Unis... in code we trust ! </p>
+
+
+            </article>
+    </section>
+
+    <div class= "col-xs-12 sectionimage" id="image1">
+    <section>
+    </section>
+    </div>
+
+    <section id="formation" class="col-xs-10 col-xs-offset-1 toutesection">
+      <header>
+        <h2 class="wow bounceInRight" data-wow-duration="3s" data-wow-delay="100ms" data-wow-offset="100">2016 - Développeur Web </h2>
+      </header>
+      <article class="contenue wow bounceInLeft" data-wow-duration="4s" data-wow-delay="100ms" data-wow-offset="100">
+        <p>
+          Cette formation m’a permis de développer des compétences
+          dans les deux aspects essentiels des langages informatiques :
+        </p>
+
+        <p>
+          Le "front-end" pour la création de la structure (HTML), la
+          mise en forme (CSS) et l'animation (JavaScript) du site.
+        </p>
+
+        <p>
+          Le "back-end" pour rendre le site dynamique via l'utilisation de
+          fonctionnalités à partir du langage informatique PHP.
+        </p>
+
+        <p>
+          De plus, l'utilisation d'outils ("framework") permet
+          d'optimiser le travail du développeur web. (Symfony pour PHP, Jquery pour JavaScript,
+          Bootstrap pour le responsive etc)
+        </p>
+
+        <p>
+          Il existe également des outils collaboratifs tels que
+          Framapad ("bloc note numérique") qui permet aux développeurs de communiquer entre eux
+          sur un projet. Le trello est un agenda qui permet d'aménager un planning de travail.</p>
+        <p>
+          Github a pour but de comiter ("enregistrer") son travail entre collaborateurs.
+        </p>
+      </article>
+    </section>
+
+    <section id="competences" class="col-xs-12 toutesection">
+      <header>
+        <h2 class="wow bounceInRight" data-wow-duration="3s" data-wow-delay="20ms" data-wow-offset="100">Compétences</h2>
+      </header>
+      <article class="tableau contenue wow bounceInLeft" data-wow-duration="3s" data-wow-delay="20ms" data-wow-offset="100">
+        <table>
+            <tr>
+              <td>
+                HTML
+              </td>
+              <td>
+                Structure de l'information
+              </td>
+            </tr>
+            <tr>
+              <td>
+                CSS
+              </td>
+              <td>
+                Mise en page
+              </td>
+            </tr>
+            <tr>
+              <td>
+                BOOTSTRAP
+              </td>
+              <td>
+                Permet de rendre responsive
+              </td>
+            <tr>
+              <td>
+                SASS
+              </td>
+              <td>
+                Pré-processeur CSS (sépare les tâches et créé des variables)
+              </td>
+            </tr>
+            <tr>
+              <td>
+                PHP
+              </td>
+              <td>
+                Rend un site dynamique (changer du contenu automatiquement)
+              </td>
+            </tr>
+          </table>
+          <!--<p>J'ai appris à faire des animations à l'aide du langage JavaScript</p>-->
+      </article>
+    </section>
+
+
+    <footer class="col-xs-12 contact" id="contact">
+      <section>
+        <article class="textecontact">
+          <p>
+            Pour me contacter vous pouvez m'envoyer un message, vous pouvez egalement me retrouver
+            sur Facebook et Twitter.
+          </p>
+        </article>
+        <div>
+          <ul class="listelien">
+            <li class="liencontact liencontactreseau wow zoomIn rotateIn" data-wow-duration="3s" data-wow-delay="20ms" data-wow-offset="100"><a href="https://www.facebook.com/jimmyfriedblatt.dev.web"><img class="photofooter" src="./css/img/facebook.png" alt="" /></a></li>
+            <li class="liencontact liencontactreseau wow zoomIn rotateIn" data-wow-duration="3s" data-wow-delay="20ms" data-wow-offset="100"><a href="https://twitter.com/Jimmy_Dev_Web"><img class="photofooter" src="./css/img/twitter.png" alt="" /></a></li>
+          </ul>
+      </div>
+
+    <div class="container">
+      <div class="starter-template">
+
+        <?php if(array_key_exists('errors', $_SESSION)): ?>
+            <div class="alert alert-danger">
+                <?= implode('<br>', $_SESSION['errors']); ?>
+            </div>
+        <?php endif; ?>
+        <?php if(array_key_exists('success', $_SESSION)): ?>
+            <div class="alert alert-success">
+              Votre email a bien été envoyé
+            </div>
+        <?php endif; ?>
+
+        <form action="post_contact.php" method="POST">
+          <div class="row">
+            <div class="col-xs-6">
+              <div class="form-group">
+                <label for="inputname">Votre nom</label>
+                <input type="text" name="name" class="form-control" id="inputname" value="<?= isset($_SESSION['inputs']['name']) ? $_SESSION['inputs']['name'] : ''; ?>">
+              </div>
+            </div>
+            <div class="col-xs-6">
+              <div class="form-group">
+                <label for="inputprenom">Votre prénom</label>
+                <input type="text" name="prenom" class="form-control" id="inputprenom" value="<?= isset($_SESSION['inputs']['prenom']) ? $_SESSION['inputs']['prenom'] : ''; ?>">
+              </div>
+            </div>
+            <div class="col-xs-6">
+              <div class="form-group">
+                <label for="inputemail">Votre email</label>
+                <input type="text" name="email" class="form-control" id="inputemail" value="<?= isset($_SESSION['inputs']['email']) ? $_SESSION['inputs']['email'] : ''; ?>">
+              </div>
+            </div>
+            <div class="col-xs-6">
+              <div class="form-group">
+                <label for="inputobjet">Objet</label>
+                <input type="text" name="objet" class="form-control" id="inputobjet" value="<?= isset($_SESSION['inputs']['objet']) ? $_SESSION['inputs']['objet'] : ''; ?>">
+              </div>
+            </div>
+            <div class="col-xs-12">
+              <div class="form-group ">
+                <label for="inputmessage">Votre message</label>
+                <textarea id="inputmessage" name="message" class="form-control champMessage"><?= isset($_SESSION['inputs']['message']) ? $_SESSION['inputs']['message'] : ''; ?></textarea>
+              </div>
+              <button type="submit" class="btn btn-primary test col-xs-2 col-xs-offset-5">Envoyer</button>
+            </div>
+          </div>
+        </form>
+
+      </div>
+    </div>
+
+     </section>
+    </footer>
+    <script src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.12.4.min.js"></script>
+    <script type="text/javascript" src="./javascript/smoothscroll.js"></script>
+    <script type="text/javascript" src="./javascript/wow.min.js"></script>
+    <script type="text/javascript" src="./javascript/initwow.js"> </script>
+
+  </body>
+</html>
+
+<?php
+unset($_SESSION['inputs']);
+unset($_SESSION['errors']);
+unset($_SESSION['success']);
+
+ ?>
